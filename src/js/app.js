4c8ceeb1bf98608da54bceade7eebd2ff4dd095e
larmflower <lnarosetta@gmail.com>
2017-05-23 13:35:31 +0100

    adding style to game

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,7 @@ $(() => {
       scrollTop: $('#myDiv').offset().top
     }, 2000);
   });
-  
+
 // Each div square has an index which is created / targeted by the click event
 // OLD CODE: ,  userAnswer = prompt corresponding question from array of objects,
 // inject content with javascript to an html elements for display of question with options
@@ -19,7 +19,12 @@ $(() => {
   const $squares = $('.div');
   const $answerText = $('.answerText');
   const $answerButtons = $('.answerButtons');
+  const $hearts = $('.hearts');
+  const $images = $('.img');
+  // const $background = $(this).css('background-image');
+
   let question = null;
+  let lives = 3;
 
   $squares.on('click', (e) => {
     const squareIndex = $(e.target).index();
@@ -43,11 +48,44 @@ $(() => {
   function checkAnswer(e) {
     if ($(e.target).text() === question.answer) {
       alert(question.correct);
+      // function here to toggle() image
     } else {
       alert(question.rebuttal);
-      // something here to deduct a life
+      removeHeart();
     }
   }
+
+  //
+  function removeHeart(){
+    lives--;
+    $hearts.eq(lives).hide();
+    if (lives === 0) gameOver();
+  }
+
+  function gameOver(){
+    console.log('game over');
+    const playAgain = confirm('Play again?');
+    if (playAgain){
+      $hearts.show();
+      lives = 3;
+    }
+  }
+
+
+// show background images on gameboard
+  // $('.block').click(function() {
+  //   $background.show();
+  // });
+
+  // function to toggle the background image on and off
+  // $('.block').toggle(
+  //   function() {
+  //       $(this).css('background-image','url('on.jpg')');
+  //   },
+  //   function() {
+  //       $(this).css('background-image','url('off.jpg')');
+  //   });
+
 
 // Create array of questions which will align with the matching indexes of divs
 // array that contains questions, answers and reactions to user input
@@ -56,105 +94,99 @@ $(() => {
     text: 'What scares Columbus more than zombies?',
     options: ['spiders', 'clowns', 'mice'],
     answer: 'clowns',
-    correct: 'correct',
-    rebuttal: 'no'
+    correct: 'That is correct! Moving right along..',
+    rebuttal: 'Sorry!, try again..'
   }, {
     text: 'The Zombie Breakout was caused by which rotten food?',
     options: ['Sushi', 'Spaghetti', 'Burgers'],
     answer: 'Burgers',
-    correct: 'correct',
-    rebuttal: 'no'
+    correct: 'Pretty gross huh? Move ahead one space!',
+    rebuttal: 'Seriously?'
   }, {
     text: 'What is Talahasse\'s\ one weakness?',
     options: ['Twinkies', 'Puppies', 'Cupcakes'],
     answer: 'Twinkies',
-    correct: 'correct',
-    rebuttal: 'no'
+    correct: 'You live to die another day..',
+    rebuttal: 'Perhaps that is your one weakness?'
   },{
     text: 'Rule #17.. Don\'\t be a what?',
     options: ['Zombie', 'Hero', 'Crook'],
     answer: 'Hero',
-    correct: 'correct',
-    rebuttal: 'no'
+    correct: 'So you\'\re doing great keep survivin and thrivin!',
+    rebuttal: 'Zombies are eating you, guess better!'
   },{
     text: 'Which famous actor does Columbus shoot for acting like a zombie?',
     options: ['Ben Affleck', 'Bill Murray', 'Brad Pitt'],
     answer: 'Bill Murray',
-    correct: 'correct',
-    rebuttal: 'no'
+    correct: 'I knew you\'\d get that one! On to the next question!',
+    rebuttal: 'If you got this wrong you deserve to be eaten by zombies.'
   }, {
     text: 'Witchita and Lil Rock are heading where?',
     options: ['The Grand Canyon', 'Disney Land', 'Pacific Playland'],
     answer: 'Pacific Playland',
-    correct: 'correct',
-    rebuttal: 'no'
+    correct: 'Well done! Please continue.',
+    rebuttal: 'NOPE! Sorry, you\'\re zombie food..'
   }, {
     text: 'Which of these rules are not on Columbus\'\ list?',
     options: ['Bounty Paper Towels', 'Always wear socks', 'Enjoy the little things'],
     answer: 'Always wear socks',
-    correct: 'correct',
-    rebuttal: 'no'
+    correct: 'You are a trivia master!',
+    rebuttal: 'Not quite but keep trying!'
   }, {
     text: 'What was the film\'\s gross three months after it released (with a budget of 23 million dollars)?',
     options: ['75 million', '1 billion', '40 million'],
     answer: '75 million',
-    correct: 'correct',
-    rebuttal: 'no'
+    correct: 'That is correct! Moving right along..',
+    rebuttal: 'I know right? WRONG. You lose a life.'
   },{
     text: 'What scares Columbus more than zombies?',
     options: ['spiders', 'clowns', 'mice'],
     answer: 'clowns',
-    correct: 'correct',
-    rebuttal: 'no'
+    correct: 'You\'\ve nearly won!',
+    rebuttal: 'Sorry!, try again..'
   }, {
     text: 'The Zombie Breakout was caused by which rotten food?',
     options: ['Sushi', 'Spaghetti', 'Burgers'],
     answer: 'Burgers',
-    correct: 'correct',
-    rebuttal: 'no'
+    correct: 'You live to die another day..',
+    rebuttal: 'Seriously?'
   }, {
     text: 'What is Talahasse\'s\ one weakness?',
     options: ['Twinkies', 'Puppies', 'Cupcakes'],
     answer: 'Twinkies',
-    correct: 'correct',
-    rebuttal: 'no'
+    correct: 'So you\'\re doing great keep survivin and thrivin!',
+    rebuttal: 'Perhaps that is your one weakness?'
   },{
     text: 'Rule #17.. Don\'\t be a what?',
     options: ['Zombie', 'Hero', 'Crook'],
     answer: 'Hero',
-    correct: 'correct',
-    rebuttal: 'no'
+    correct: 'I knew you\'\d get that one! On to the next question!',
+    rebuttal: 'Zombies are eating you, guess better!'
   },{
     text: 'Which famous actor does Columbus shoot for acting like a zombie?',
     options: ['Ben Affleck', 'Bill Murray', 'Brad Pitt'],
     answer: 'Bill Murray',
-    correct: 'correct',
-    rebuttal: 'no'
+    correct: 'Well done! Please continue.',
+    rebuttal: 'If you got this wrong you deserve to be eaten by zombies.'
   }, {
     text: 'Witchita and Lil Rock are heading where?',
     options: ['The Grand Canyon', 'Disney Land', 'Pacific Playland'],
     answer: 'Pacific Playland',
-    correct: 'correct',
-    rebuttal: 'no'
+    correct: 'You are a trivia master!',
+    rebuttal: 'NOPE! Sorry, you\'\re zombie food..'
   }, {
     text: 'Which of these rules are not on Columbus\'\ list?',
     options: ['Bounty Paper Towels', 'Always wear socks', 'Enjoy the little things'],
     answer: 'Always wear socks',
-    correct: 'correct',
-    rebuttal: 'no'
+    correct: 'That is correct! Moving right along..',
+    rebuttal: 'Not quite but keep trying!'
   }, {
     text: 'What was the film\'\s gross three months after it released (with a budget of 23 million dollars)?',
     options: ['75 million', '1 billion', '40 million'],
     answer: '75 million',
-    correct: 'correct',
-    rebuttal: 'no'
+    correct: 'You\'\ve nearly won!',
+    rebuttal: 'I know right? WRONG. You lose a life.'
   }];
-
-
-
-
-
-
 
 
 
